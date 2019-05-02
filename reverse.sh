fa438554b38c6028089d3d901239e2222acb2e8e
rosefrancis05 <rosefrancis2021@cs.ajce.in>
2019-05-02 12:01:40 +0530

    initial commit

---
+++ reverse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read no
+((b=$no))
+rev=0
+while((no>0))
+do
+ ((rem=$no%10))
+ ((rev=($rev*10)+$rem))
+ ((no=$no/10))
+done
+echo reversed number is $rev
+if(($rev==$b))
+then
+ echo No is a pallindrome
+else
+ echo No is not a pallindrome
+fi
